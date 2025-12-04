a764651bcddcdf3296a105bbc21fab135df01423
sherlock2259 <1485212259@qq.com>
2025-12-04 21:01:48 +0800

    Update HUDCaching.java

--- src/main/java/com/gtnewhorizons/angelica/hudcaching/HUDCaching.java
+++ src/main/java/com/gtnewhorizons/angelica/hudcaching/HUDCaching.java
@@ -14,17 +14,30 @@ import com.gtnewhorizons.angelica.mixins.interfaces.GuiIngameAccessor;
 import com.gtnewhorizons.angelica.mixins.interfaces.GuiIngameForgeAccessor;
 import com.gtnewhorizons.angelica.mixins.interfaces.RenderGameOverlayEventAccessor;
 import com.kentington.thaumichorizons.common.ThaumicHorizons;
+import cpw.mods.fml.common.eventhandler.EventPriority;
+import net.minecraftforge.client.event.RenderGameOverlayEvent;
+import net.minecraftforge.event.world.WorldEvent;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL30;
+
+import com.gtnewhorizons.angelica.glsm.GLStateManager;
+
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiIngame;
 import net.minecraft.client.gui.ScaledResolution;
 import net.minecraft.client.renderer.EntityRenderer;
 import net.minecraft.client.renderer.OpenGlHelper;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.shader.Framebuffer;
 import net.minecraftforge.client.GuiIngameForge;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import org.lwjgl.opengl.GL11;
 import thaumcraft.common.Thaumcraft;
 import xaero.common.core.XaeroMinimapCore;
+
+import static com.gtnewhorizons.angelica.loading.AngelicaTweaker.LOGGER;
 
 // See LICENSE-HUDCaching.md for license information.
 
@@ -57,17 +70,38 @@ public class HUDCaching {
     private final static RenderGameOverlayEvent fakeTextEvent = new RenderGameOverlayEvent.Text(new RenderGameOverlayEvent(0, null, 0, 0), null, null);
     private final static RenderGameOverlayEvent.Post fakePostEvent = new RenderGameOverlayEvent.Post(new RenderGameOverlayEvent(0, null, 0, 0), RenderGameOverlayEvent.ElementType.HELMET);
 
-    public static void init() {
-        framebuffer = new SharedDepthFramebuffer(CustomFramebuffer.STENCIL_BUFFER);
+    public static final HUDCaching INSTANCE = new HUDCaching();
+
+    private HUDCaching() {}
+
+    // 保留WorldEvent.Load监听器，确保帧缓冲在游戏开始时初始化
+    @SubscribeEvent(priority = EventPriority.HIGH)
+    public void onJoinWorld(WorldEvent.Load event) {
+        if (event.world.isRemote){
+            LOGGER.info("World loaded - Initializing HUDCaching");
+            init();
+        }
     }
 
+    public static void init() {
+        if (framebuffer == null) {
+            framebuffer = new SharedDepthFramebuffer(CustomFramebuffer.STENCIL_BUFFER);
+        }
+    }
+
+    @SuppressWarnings("unused")
     public static void renderCachedHud(EntityRenderer renderer, GuiIngame ingame, float partialTicks, boolean hasScreen, int mouseX, int mouseY) {
+        // 确保帧缓冲已初始化
+        if (framebuffer == null) {
+            init();
+        }
+        
         if (ModStatus.isXaerosMinimapLoaded && ingame instanceof GuiIngameForge) {
             // this used to be called by asming into renderGameOverlay, but we removed it
             XaeroMinimapCore.beforeIngameGuiRender(partialTicks);
         }
 
-        if (!OpenGlHelper.isFramebufferEnabled()) {
+        if (!OpenGlHelper.isFramebufferEnabled() || framebuffer == null) {
             ingame.renderGameOverlay(partialTicks, hasScreen, mouseX, mouseY);
             return;
         }
@@ -79,11 +113,21 @@ public class HUDCaching {
         if (dirty) {
             dirty = false;
             nextHudRefresh = System.currentTimeMillis() + (1000 / AngelicaConfig.hudCachingFPS);
+            
+            // 设置帧缓冲
             if (framebuffer.framebufferWidth != mc.displayWidth || framebuffer.framebufferHeight != mc.displayHeight) {
                 framebuffer.createBindFramebuffer(mc.displayWidth, mc.displayHeight);
             } else {
                 framebuffer.clearBindFramebuffer();
             }
+            
+            // 确保深度测试和模板测试正确启用
+            GLStateManager.enableDepthTest();
+            GLStateManager.enableStencilTest();
+            GLStateManager.glDepthMask(true);
+            GLStateManager.glClearDepth(1.0);
+            GLStateManager.glClear(GL11.GL_DEPTH_BUFFER_BIT | GL11.GL_STENCIL_BUFFER_BIT);
+            
             renderingCacheOverride = true;
             ingame.renderGameOverlay(partialTicks, hasScreen, mouseX, mouseY);
             renderingCacheOverride = false;
@@ -110,6 +154,139 @@ public class HUDCaching {
 
         if (ingame instanceof GuiIngameForge) {
             GuiIngameForgeAccessor guiForge = ((GuiIngameForgeAccessor) ingame);
+            if (renderHelmetCaptured) {
+                guiForge.callRenderHelmet(resolution, partialTicks, hasScreen, mouseX, mouseY);
+                if (ModStatus.isHoloInventoryLoaded) {
+                    HoloInventoryReflectionCompat.setAngelicaOverride(false);
+                    // only settings the partial ticks as mouseX and mouseY are not used in renderEvent
+                    ((RenderGameOverlayEventAccessor) fakePostEvent).setPartialTicks(partialTicks);
+                    HoloInventoryReflectionCompat.renderEvent(fakePostEvent);
+                }
+            }
+            if (renderPortalCapturedTicks > 0) {
+                guiForge.callRenderPortal(width, height, partialTicks);
+            }
+            if (renderCrosshairsCaptured) {
+                if (ModStatus.isXaerosMinimapLoaded) {
+                    // this fixes the crosshair going invisible when no lines are being drawn under the minimap
+                    GLStateManager.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+                }
+                guiForge.callRenderCrosshairs(width, height);
+            }
+            if (ModStatus.isThaumcraftLoaded || ModStatus.isThaumicHorizonsLoaded) {
+                ((RenderGameOverlayEventAccessor) fakeTextEvent).setPartialTicks(partialTicks);
+                ((RenderGameOverlayEventAccessor) fakeTextEvent).setResolution(resolution);
+                ((RenderGameOverlayEventAccessor) fakeTextEvent).setMouseX(mouseX);
+                ((RenderGameOverlayEventAccessor) fakeTextEvent).setMouseY(mouseY);
+                if (ModStatus.isThaumcraftLoaded) {
+                    Thaumcraft.instance.renderEventHandler.renderOverlay(fakeTextEvent);
+                }
+                if (ModStatus.isThaumicHorizonsLoaded) {
+                    ThaumicHorizons.instance.renderEventHandler.renderOverlay(fakeTextEvent);
+                }
+            }
+        } else {
+            if (renderHelmetCaptured) {
+                gui.callRenderPumpkinBlur(width, height);
+            }
+            if (renderPortalCapturedTicks > 0) {
+                gui.callRenderPortal(renderPortalCapturedTicks, width, height);
+            }
+        }
+
+        // render cached frame
+        GLStateManager.enableBlend();
+        GLStateManager.tryBlendFuncSeparate(GL11.GL_ONE, GL11.GL_ONE_MINUS_SRC_ALPHA, GL11.GL_ONE, GL11.GL_ONE_MINUS_SRC_ALPHA);
+        GLStateManager.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+        framebuffer.bindFramebufferTexture();
+        // 设置纹理参数，确保渲染正确
+        GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_NEAREST);
+        GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_NEAREST);
+        drawTexturedRect((float) resolution.getScaledWidth_double(), (float) resolution.getScaledHeight_double());
+
+        GLStateManager.tryBlendFuncSeparate(GL11.GL_ONE, GL11.GL_ONE_MINUS_SRC_ALPHA, GL11.GL_ONE, GL11.GL_ZERO);
+        mc.getTextureManager().bindTexture(Gui.icons);
+    }
+
+    /**
+     * We are skipping certain render calls when rendering into cache,
+     * however, we cannot skip the GL state changes. This will fix
+     * the state before we start rendering
+     */
+    public static void fixGLStateBeforeRenderingCache() {
+        GLStateManager.glDepthMask(true);
+        GLStateManager.enableDepthTest();
+        GLStateManager.enableAlphaTest();
+        GLStateManager.tryBlendFuncSeparate(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA, GL11.GL_ONE, GL11.GL_ZERO);
+        GLStateManager.disableBlend();
+    }
+
+    private static void drawTexturedRect(float width, float height) {
+        if (quadWidth != width || quadHeight != height) {
+            quadWidth = width;
+            quadHeight = height;
+            if (quadVAO != null) {
+                quadVAO.close();
+            }
+            quadVAO = rebuildVAO(width, height);
+        }
+        
+        // 保存当前状态
+        boolean depthTest = GL11.glGetBoolean(GL11.GL_DEPTH_TEST);
+        boolean stencilTest = GL11.glGetBoolean(GL11.GL_STENCIL_TEST);
+        boolean blend = GL11.glGetBoolean(GL11.GL_BLEND);
+        
+        // 渲染缓存纹理时禁用深度和模板测试
+        GLStateManager.disableDepthTest();
+        GLStateManager.disableStencilTest();
+        GLStateManager.enableTexture();
+        GLStateManager.enableBlend();
+        
+        quadVAO.render();
+        
+        // 恢复状态
+        if (depthTest) {
+            GLStateManager.enableDepthTest();
+        } else {
+            GLStateManager.disableDepthTest();
+        }
+        
+        if (stencilTest) {
+            GLStateManager.enableStencilTest();
+        } else {
+            GLStateManager.disableStencilTest();
+        }
+        
+        if (!blend) {
+            GLStateManager.disableBlend();
+        }
+    }
+
+
+    private static VertexBuffer rebuildVAO(float width, float height) {
+        final CapturingTessellator tessellator = TessellatorManager.startCapturingAndGet();
+        tessellator.startDrawingQuads();
+        tessellator.addVertexWithUV(0, height, 0.0, 0, 0);
+        tessellator.addVertexWithUV(width, height, 0.0, 1, 0);
+        tessellator.addVertexWithUV(width, 0, 0.0, 1, 1);
+        tessellator.addVertexWithUV(0, 0, 0.0, 0, 1);
+        tessellator.draw();
+        return TessellatorManager.stopCapturingToVAO(DefaultVertexFormat.POSITION_TEXTURE);
+    }
+
+    public static void disableHoloInventory() {
+        if (ModStatus.isHoloInventoryLoaded) {
+            HoloInventoryReflectionCompat.setAngelicaOverride(true);
+        }
+    }
+
+    @SuppressWarnings("unused") // called via ASM
+    public static class HUDCachingHooks {
+        public static boolean shouldReturnEarly() {
+            return renderingCacheOverride;
+        }
+    }
+}            GuiIngameForgeAccessor guiForge = ((GuiIngameForgeAccessor) ingame);
             if (renderHelmetCaptured) {
                 guiForge.callRenderHelmet(resolution, partialTicks, hasScreen, mouseX, mouseY);
                 if (ModStatus.isHoloInventoryLoaded) {
